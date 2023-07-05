e2f39d8baa14e49db68ce1aa558e201eeaa97ef1
Rahul verma <rahulverma11071998@gmail.com>
2023-07-05 13:52:11 +0530

    initial commit

---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const ContentWrapper = ({ children }) => {
+    return <div className="w-full max-w-[1200px] mx-auto my-0 px-[20px] py-0">{children}</div>;
+};
+
+export default ContentWrapper;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContentWrapper from './ContentWrapper'
+
+const Header = () => {
+  return (
+    <Header>
+    <ContentWrapper>
+        <div className='flex '>
+
+        </div>
+    </ContentWrapper>
+    
+    
+    </Header>
+  )
+}
+
+export default Header
